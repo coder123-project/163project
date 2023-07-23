688662a93454fc180ee7aacfeb842a75598c664d
coder123-project <83872425+coder123-project@users.noreply.github.com>
2023-07-23 10:53:05 -0700

    Add files via upload

---
+++ playerMovement.js
@@ -0,0 +1,15 @@
+AFRAME.registerComponent("player-movement", {
+  init: function () {
+    this.walk();
+  },
+  walk: function () {
+    window.addEventListener("keydown", (e) => {
+    //add the code to play sound as per selection of arrow keys for movement
+
+
+
+
+    });
+  },
+});
+
